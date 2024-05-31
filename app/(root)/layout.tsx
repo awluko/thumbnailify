e9080d1d7ef8899ad06f7f921b02ee92d568d4cc
Andrew <andrew.w.lukosevic@gmail.com>
2024-05-31 07:37:09 -0500

    Added Clerk and messed with the sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,16 @@
+import Sidebar from '@/components/shared/Sidebar'
 import React from 'react'
 
 const layout = ({children}: { children: React.ReactNode }) => {
   return (
     <main className="root">
-        <div className="root-container">
-            <div className="wrapper">
-                {children}
-            </div>
-        </div>
+      <Sidebar />
+      {/* < MobileNave*/}
+      <div className="root-container">
+          <div className="wrapper">
+              {children}
+          </div>
+      </div>
     </main>
   )
 }
